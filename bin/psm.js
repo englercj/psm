f33a57309bf51cbe60757c9d9dca68700e905b73
Chad Engler <englercj@live.com>
2012-04-09 16:19:04 -0400

    Updated logging, removing logs from repo

--- bin/psm.js
+++ bin/psm.js
@@ -10,55 +10,72 @@ path = require('path'),
 colors = require('colors'),
 fs = require('fs'),
 rl = require('readline').createInterface(process.stdin, process.stdout),
+config = JSON.parse(fs.readFileSync(path.join(__dirname, '../config.json')));
 
-servers = JSON.parse(fs.readFileSync(path.join(__dirname, '../servers.json'))),
+colors.setTheme(config.colors);
+
+var servers = JSON.parse(fs.readFileSync(path.join(__dirname, '../servers.json'))),
 psm = new Psm(servers),
-prompt = 'psm'.cyan + '> '.green,
+prompt = 'psm'.promptText + '> '.prompt,
 promptLen = 5,
-welcome = 
-    "________ ".red  + "________".white + "______  ___\n".blue +
-    "___  __ \\".red + "__  ___/".white + "___   |/  /\n".blue +
-    "__  /_/ /".red  + "_____ \\".white + " __  /|_/ / \n".blue +
-    "_  ____/ ".red  + "____/ / ".white + "_  /  / /  \n".blue +
-    "/_/      ".red  + "/____/  ".white + "/_/  /_/   \n".blue,
+welcome = "\n" +
+    "________ ".P  + "________".S + "______  ___\n".M +
+    "___  __ \\".P + "__  ___/".S + "___   |/  /\n".M +
+    "__  /_/ /".P  + "_____ \\".S + " __  /|_/ / \n".M +
+    "_  ____/ ".P  + "____/ / ".S + "_  /  / /  \n".M +
+    "/_/      ".P  + "/____/  ".S + "/_/  /_/   \n".M,
+
+//built in commands
 cmds = {
-    echo: echo, help: help, exit: exit
+    help: {
+	man: 'displays this help message',
+	func: function() {
+	    log('Available Commands:');
+	    log('-------------------');
+	    for(var cmd in cmds) {
+		if(cmds.hasOwnProperty(cmd)) {
+		    log(cmd.helpCmd + ' - '.helpDash + cmds[cmd].man.helpMan);
+		}
+	    }
+	}
+    },
+    echo: {
+	man: 'displays a string of text',
+	func: function(line) { 
+	    log(line.substr(line.indexOf(' ') + 1)); 
+	}
+    },
+    exit: {
+	man: 'exits the CLI',
+	func: function() {
+	    log('Good bye!');
+	    process.exit(0);
+	}
+    }
 };
 
 //CLI processing
-console.log(welcome);
-console.log('Panther Server Manager CLI has started.'.green);
-console.log('Type "help" for a command list'.yellow);
+log(welcome);
+log('Panther Server Manager CLI has started.'.startMessage);
+log('Type "help" for a command list'.typeHelpMessage);
 
 //process each line
 rl.on('line', function(line) {
     line = line.trim();
+    log(prompt + line, false);
+
     if(line) {
 	var cmd = line.split(' ')[0];
 	
 	if(cmds[cmd]) 
-	    cmds[cmd](line);
+	    cmds[cmd].func(line);
 	else
 	    log('Unkown command, try "help"');
     }
 
     rl.prompt();
-}).on('close', exit);
+}).on('close', cmds.exit.func);
 
 //start prompting
 rl.setPrompt(prompt, promptLen);
 rl.prompt();
-
-//built in functions
-function help() {
-    
-}
-
-function echo(line) { 
-    log(line.substr(line.indexOf(' ') + 1)); 
-}
-
-function exit() {
-    log('Good bye!');
-    process.exit(0);
-}
